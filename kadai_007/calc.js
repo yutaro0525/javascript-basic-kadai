6f37b715a6db6c4eaeadb45d7191f573adf67085
yutaro0525 <148600619+yutaro0525@users.noreply.github.com>
2023-12-02 10:02:36 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,22 @@
+//ランダムな整数（12まで）
+let num = Math.floor(Math.random() * 16);
+
+//数値確認用
+console.log(num);
+
+if (num === 3 || num === 6 || num === 9 || num === 12){
+  console.log('3の倍数です');
+}
+
+else if (num === 5 || num === 10){
+  console.log('5の倍数です');
+}
+
+else if (num === 15){
+  console.log('3と5の倍数です');
+}
+
+else {
+  console.log(num);
+}
+
