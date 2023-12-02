9d7a6fbe3f298d216b954df9ac63b1bc43c9c9a0
yutaro0525 <148600619+yutaro0525@users.noreply.github.com>
2023-12-02 10:32:29 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -6,7 +6,11 @@ let height;
 
 height  = 1.7;
 
+let BMI;
+
+BMI = (weight)/(height*height);
+
 
 console.log('weight'+weight);
 console.log('height'+height);
-console.log('BMI'+(weight)/(height*height));
+console.log('BMI'+BMI);
